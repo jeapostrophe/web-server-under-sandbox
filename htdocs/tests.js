3e4a94d8953738c27c4cdae38f602569bb5ebb45
Danny Yoo <dyoo@hashcollision.org>
2013-04-20 11:50:21 -0600

    README update

--- htdocs/tests.js
+++ htdocs/tests.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function() {
     "use strict";    
+    var i = 0;
     // Torture test: keep rerunning the tests over and over.
     plt.tests.initTests(function(runTests) {
         var k = function() {
+            i = i + 1;
             $("#is-running").text(
-                "Tests finished.  " +
+                "Pass " + i + ": " +
                     plt.tests.getTestsRunCount() + " tests executed."); 
             setTimeout(
                 function() {
