da072d1173dff121d75a86310d50f648c13c3057
Danny Yoo <dyoo@hashcollision.org>
2013-04-19 22:40:01 -0600

    first commit

---
+++ server.rkt
@@ -0,0 +1,34 @@
+#lang racket/base
+
+(require web-server/servlet-env
+         web-server/servlet
+         racket/runtime-path
+         (for-syntax racket/base))
+
+(provide start-server)
+
+(define-runtime-path htdocs (build-path "htdocs"))
+
+(define (start req)
+  #"Ok")
+
+(define (start-server #:port [port 8000]
+                      #:listen-ip [listen-ip "127.0.0.1"])
+    (thread (lambda ()
+              (printf "starting web server on port ~s\n" port)
+              (serve/servlet start 
+                             #:listen-ip listen-ip
+                             #:servlet-path "/compile"
+                             #:extra-files-paths (list htdocs)
+                             #:launch-browser? #f
+                             #:port port))))
+
+(module+ main
+  (define current-port (make-parameter 8080))
+  (require racket/cmdline)
+  (void (command-line
+         #:once-each 
+         [("-p" "--port") p "Port (default 8000)" 
+          (current-port (string->number p))]))
+  (sync (start-server #:port (current-port))))
+  
